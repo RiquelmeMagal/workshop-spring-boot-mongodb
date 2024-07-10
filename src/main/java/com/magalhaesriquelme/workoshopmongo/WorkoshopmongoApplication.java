ed6e9a44e9f73af8c3b930d0e7fcaa6203dc53ad
Riquelme Magalhaes <riquelme.souza@arapiraca.ufal.br>
2024-07-10 09:28:40 -0300

    feat: Criação do projeto.

---
+++ src/main/java/com/magalhaesriquelme/workoshopmongo/WorkoshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.magalhaesriquelme.workoshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magalhaesriquelme/workoshopmongo/WorkoshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalhaesriquelme.workoshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
