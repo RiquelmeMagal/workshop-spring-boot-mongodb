63d8382bb28476f955df709293ac89ddcd7e4366
Riquelme Magalhaes <riquelme.souza@arapiraca.ufal.br>
2024-07-12 13:21:36 -0300

    feat: Obtendo um usuário por id

--- src/main/java/com/magalhaesriquelme/workoshopmongo/service/UserService.java
+++ src/main/java/com/magalhaesriquelme/workoshopmongo/service/UserService.java
@@ -3,18 +3,25 @@ package com.magalhaesriquelme.workoshopmongo.service;
 
 import com.magalhaesriquelme.workoshopmongo.domain.User;
 import com.magalhaesriquelme.workoshopmongo.repository.UserRepository;
+import com.magalhaesriquelme.workoshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repo;
-    
-    public List<User> FindAll(){
+
+    public List<User> FindAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
